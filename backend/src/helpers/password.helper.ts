08d4cbc021b7fd7f08d5fc6a9c2203d50eebe9d8
volesh <volesh2@gmail.com>
2023-02-11 19:20:10 +0200

    finish

--- backend/src/helpers/password.helper.ts
+++ backend/src/helpers/password.helper.ts
@@ -1,9 +1,7 @@
 import * as argon2 from 'argon2';
-import { createHmac } from 'crypto';
 
 export const passwordHelper = {
   hashPassword: async (password: string): Promise<string> => {
-    const hashedPass = await argon2.hash(password);
-    return createHmac('AES256-CBC', hashedPass).digest('hex');
+    return argon2.hash(password);
   }
 };
